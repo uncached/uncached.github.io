d38ed823eb087bfecb7fdc122ff41cd7dd6409f2
uncached <i.sheonet@gmail.com>
2017-03-16 22:33:11 +0700

    Update randombg.js

--- js/randombg.js
+++ js/randombg.js
@@ -7,10 +7,9 @@ var backgrounds = new Array(
 var current = 0;
 
 function nextBackground() {
-body.css(
-'background',
-backgrounds[current = ++current % backgrounds.length]
-);
+body.css({
+'background':backgrounds[current = ++current % backgrounds.length], 'background-color': 'grey', 'background-blend-mode': 'multiply'
+});
 
 setTimeout(nextBackground, 15000);
 }
