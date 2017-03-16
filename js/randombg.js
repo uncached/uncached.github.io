41cd92cebb7827fd8a2f756c19528b1c8f2fd68d
uncached <i.sheonet@gmail.com>
2017-03-16 21:55:04 +0700

    Update randombg.js

--- js/randombg.js
+++ js/randombg.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	var img = Math.floor((Math.random()*4)+1);
 	var bg = 'url(https://github.toiyeuwp.com/bg-img/' + img + '.jpg)';
-	$('body').css({'background':bg, 'background-size':'cover', });
+	$('body').css({'background':bg 'no-repeat center center fixed', '-webkit-background-size':'cover', '-moz-background-size':'cover', '-o-background-size':'cover', 'background-size':'cover', 'background-color':'grey', 'background-blend-mode':'multiply' });
 });
