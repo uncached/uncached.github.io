c46405659651154bb5055583c5bd11817b3467f2
uncached <i.sheonet@gmail.com>
2017-03-16 22:17:01 +0700

    Update randombg.js

--- js/randombg.js
+++ js/randombg.js
@@ -1,5 +1,19 @@
-$(document).ready(function() {
-	var img = Math.floor((Math.random()*4)+1);
-	var bg = 'url(https://github.toiyeuwp.com/bg-img/' + img + '.jpg)';
-	$('body').css({'background':bg });
+$(function() {
+var body = $('body');
+var backgrounds = new Array(
+'url(https://github.toiyeuwp.com/bg-img/1.jpg)',
+'url(https://github.toiyeuwp.com/bg-img/2.jpg)'
+);
+var current = 0;
+
+function nextBackground() {
+body.css(
+'background',
+backgrounds[current = ++current % backgrounds.length]
+);
+
+setTimeout(nextBackground, 5000);
+}
+setTimeout(nextBackground, 5000);
+body.css('background', backgrounds[0]);
 });
