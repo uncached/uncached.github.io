e856e61092316299857837ba7fe708c3a5803f24
uncached <i.sheonet@gmail.com>
2017-03-27 17:07:46 +0700

    Update randombg.js

--- js/randombg.js
+++ js/randombg.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
 	var bgImgs = [
-url('https://nodejs.vn/uploads/files/1490606729205-1.jpg'),
-url('https://nodejs.vn/uploads/files/1490606755733-10.jpg'),
-url('https://nodejs.vn/uploads/files/1490606773206-11.jpg'),
-url('https://nodejs.vn/uploads/files/1490606785815-9.jpg'),
-url('https://nodejs.vn/uploads/files/1490606797991-6.jpg'),
-url('https://nodejs.vn/uploads/files/1490606809707-5.jpg'),
-url('https://nodejs.vn/uploads/files/1490606820692-4.jpg'),
-url('https://nodejs.vn/uploads/files/1490606836348-3.jpg'),
-url('https://nodejs.vn/uploads/files/1490606857006-7.jpg'),
+'url("https://nodejs.vn/uploads/files/1490606729205-1.jpg")',
+'url("https://nodejs.vn/uploads/files/1490606755733-10.jpg")',
+'url("https://nodejs.vn/uploads/files/1490606773206-11.jpg")',
+'url("https://nodejs.vn/uploads/files/1490606785815-9.jpg")',
+'url("https://nodejs.vn/uploads/files/1490606797991-6.jpg")',
+'url("https://nodejs.vn/uploads/files/1490606809707-5.jpg")',
+'url("https://nodejs.vn/uploads/files/1490606820692-4.jpg")',
+'url("https://nodejs.vn/uploads/files/1490606836348-3.jpg")',
+'url("https://nodejs.vn/uploads/files/1490606857006-7.jpg")'
 	];
-	var bgId = Math.floor((Math.random()*9));
-	var bgImg = bgImgs[bgId];
-	$('body').css({'background':bgImg, 'background-size':'cover', });
+	var bgId = Math.floor(Math.random()*9);
+	$('body').css({'background':bgImgs[bgId], 'background-size':'cover', });
 });
